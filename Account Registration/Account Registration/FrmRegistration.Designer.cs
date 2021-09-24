35297026b87fc6b66beca55b3c98e4371a1d99f1
Zomer <zomerzxc.19@gmail.com>
2021-09-25 06:16:54 +0800

    Finish the registration UI

---
+++ Account Registration/Account Registration/FrmRegistration.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Account_Registration
+{
+    partial class FrmRegistration
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+             this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Account Registration";
+            
+            this.studentNumberLabel = new System.Windows.Forms.Label();
+            this.studentNumberTextbox = new System.Windows.Forms.TextBox();
+            this.programTextbox = new System.Windows.Forms.TextBox();
+            this.programLabel = new System.Windows.Forms.Label();
+            this.lastNameTextbox = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstNameTextbox = new System.Windows.Forms.TextBox();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.middleNameTextbox = new System.Windows.Forms.TextBox();
+            this.middleNameLabel = new System.Windows.Forms.Label();
+            this.contactNumberTextbox = new System.Windows.Forms.TextBox();
+            this.contactNumberLabel = new System.Windows.Forms.Label();
+            this.ageTextbox = new System.Windows.Forms.TextBox();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.addressRichTextbox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // studentNumberLabel
+            // 
+            this.studentNumberLabel.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.studentNumberLabel.Location = new System.Drawing.Point(17, 25);
+            this.studentNumberLabel.Name = "studentNumberLabel";
+            this.studentNumberLabel.Size = new System.Drawing.Size(156, 22);
+            this.studentNumberLabel.TabIndex = 0;
+            this.studentNumberLabel.Text = "Student No.:\r\n";
+            // 
+            // studentNumberTextbox
+            // 
+            this.studentNumberTextbox.Font = new System.Drawing.Font("Open Sans SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.studentNumberTextbox.Location = new System.Drawing.Point(17, 51);
+            this.studentNumberTextbox.Name = "studentNumberTextbox";
+            this.studentNumberTextbox.Size = new System.Drawing.Size(163, 20);
+            this.studentNumberTextbox.TabIndex = 1;
+            // 
+            // programTextbox
+            // 
+            this.programTextbox.Font = new System.Drawing.Font("Open Sans SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.programTextbox.Location = new System.Drawing.Point(184, 51);
+            this.programTextbox.Name = "programTextbox";
+            this.programTextbox.Size = new System.Drawing.Size(210, 20);
+            this.programTextbox.TabIndex = 3;
+            // 
+            // programLabel
+            // 
+            this.programLabel.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.programLabel.Location = new System.Drawing.Point(179, 25);
+            this.programLabel.Name = "programLabel";
+            this.programLabel.Size = new System.Drawing.Size(161, 22);
+            this.programLabel.TabIndex = 2;
+            this.programLabel.Text = "Program:";
+            // 
+            // lastNameTextbox
+            // 
+            this.lastNameTextbox.Font = new System.Drawing.Font("Open Sans SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lastNameTextbox.Location = new System.Drawing.Point(17, 111);
+            this.lastNameTextbox.Name = "lastNameTextbox";
+            this.lastNameTextbox.Size = new System.Drawing.Size(163, 20);
+            this.lastNameTextbox.TabIndex = 5;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lastNameLabel.Location = new System.Drawing.Point(17, 85);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(156, 22);
+            this.lastNameLabel.TabIndex = 4;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // firstNameTextbox
+            // 
+            this.firstNameTextbox.Font = new System.Drawing.Font("Open Sans SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.firstNameTextbox.Location = new System.Drawing.Point(186, 111);
+            this.firstNameTextbox.Name = "firstNameTextbox";
+            this.firstNameTextbox.Size = new System.Drawing.Size(163, 20);
+            this.firstNameTextbox.TabIndex = 7;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.firstNameLabel.Location = new System.Drawing.Point(186, 85);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(156, 22);
+            this.firstNameLabel.TabIndex = 6;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // middleNameTextbox
+            // 
+            this.middleNameTextbox.Font = new System.Drawing.Font("Open Sans SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.middleNameTextbox.Location = new System.Drawing.Point(355, 111);
+            this.middleNameTextbox.Name = "middleNameTextbox";
+            this.middleNameTextbox.Size = new System.Drawing.Size(163, 20);
+            this.middleNameTextbox.TabIndex = 9;
+            // 
+            // middleNameLabel
+            // 
+            this.middleNameLabel.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.middleNameLabel.Location = new System.Drawing.Point(355, 85);
+            this.middleNameLabel.Name = "middleNameLabel";
+            this.middleNameLabel.Size = new System.Drawing.Size(156, 22);
+            this.middleNameLabel.TabIndex = 8;
+            this.middleNameLabel.Text = "Middle Name";
+            // 
+            // contactNumberTextbox
+            // 
+            this.contactNumberTextbox.Font = new System.Drawing.Font("Open Sans SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.contactNumberTextbox.Location = new System.Drawing.Point(186, 174);
+            this.contactNumberTextbox.Name = "contactNumberTextbox";
+            this.contactNumberTextbox.Size = new System.Drawing.Size(163, 20);
+            this.contactNumberTextbox.TabIndex = 13;
+            // 
+            // contactNumberLabel
+            // 
+            this.contactNumberLabel.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.contactNumberLabel.Location = new System.Drawing.Point(186, 148);
+            this.contactNumberLabel.Name = "contactNumberLabel";
+            this.contactNumberLabel.Size = new System.Drawing.Size(156, 22);
+            this.contactNumberLabel.TabIndex = 12;
+            this.contactNumberLabel.Text = "Contact No.:";
+            // 
+            // ageTextbox
+            // 
+            this.ageTextbox.Font = new System.Drawing.Font("Open Sans SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.ageTextbox.Location = new System.Drawing.Point(17, 174);
+            this.ageTextbox.Name = "ageTextbox";
+            this.ageTextbox.Size = new System.Drawing.Size(163, 20);
+            this.ageTextbox.TabIndex = 11;
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.ageLabel.Location = new System.Drawing.Point(17, 148);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(156, 22);
+            this.ageLabel.TabIndex = 10;
+            this.ageLabel.Text = "Age:";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.addressLabel.Location = new System.Drawing.Point(17, 217);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(156, 22);
+            this.addressLabel.TabIndex = 14;
+            this.addressLabel.Text = "Address:";
+            // 
+            // addressRichTextbox
+            // 
+            this.addressRichTextbox.Font = new System.Drawing.Font("Open Sans SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.addressRichTextbox.Location = new System.Drawing.Point(21, 247);
+            this.addressRichTextbox.Name = "addressRichTextbox";
+            this.addressRichTextbox.Size = new System.Drawing.Size(496, 132);
+            this.addressRichTextbox.TabIndex = 15;
+            this.addressRichTextbox.Text = "";
+            // 
+            // FrmRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(541, 409);
+            this.Controls.Add(this.addressRichTextbox);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.contactNumberTextbox);
+            this.Controls.Add(this.contactNumberLabel);
+            this.Controls.Add(this.ageTextbox);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.middleNameTextbox);
+            this.Controls.Add(this.middleNameLabel);
+            this.Controls.Add(this.firstNameTextbox);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.lastNameTextbox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.programTextbox);
+            this.Controls.Add(this.programLabel);
+            this.Controls.Add(this.studentNumberTextbox);
+            this.Controls.Add(this.studentNumberLabel);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.MaximumSize = new System.Drawing.Size(557, 448);
+            this.MinimumSize = new System.Drawing.Size(557, 448);
+            this.Name = "FrmRegistration";
+            this.Load += new System.EventHandler(this.FrmRegistration_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+        
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.RichTextBox addressRichTextbox;
+
+        private System.Windows.Forms.TextBox ageTextbox;
+        private System.Windows.Forms.Label ageLabel;
+        
+        private System.Windows.Forms.TextBox contactNumberTextbox;
+        private System.Windows.Forms.Label contactNumberLabel;
+
+        private System.Windows.Forms.Label studentNumberLabel;
+        private System.Windows.Forms.TextBox studentNumberTextbox;
+        
+        private System.Windows.Forms.TextBox programTextbox;
+        private System.Windows.Forms.Label programLabel;
+        
+        private System.Windows.Forms.TextBox firstNameTextbox;
+        private System.Windows.Forms.Label firstNameLabel;
+        
+        private System.Windows.Forms.TextBox lastNameTextbox;
+        private System.Windows.Forms.Label lastNameLabel;
+
+        private System.Windows.Forms.TextBox middleNameTextbox;
+        private System.Windows.Forms.Label middleNameLabel;
+
+        #endregion
+    }
+}
