ec794e588d59c572e84fa065a0580333bb123f92
Zomer <zomerzxc.19@gmail.com>
2021-10-09 16:15:07 +0800

    Add another activities

---
+++ DataTypeApp/DataTypeApp/DataTypeProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataTypeApp
+{
+    class DataTypeProgram
+    {
+        static void Main(string[] args)
+        {
+            String firstName = "Zomer";
+            int age = 23;
+            char middleInitial = 'L';
+            double GWA = 1.75;
+            bool isMale = true;
+            
+            Console.WriteLine(String.Format("First name: {0}\nAge: {1}\nMiddle initial: {2}\nGWA: {3}\nIs male? {4}",
+                firstName, age, middleInitial, GWA, isMale));
+        }
+    }
+}
---
+++ MathApp/MathApp/MathProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MathApp
+{
+    class MathProgram
+    {
+        static void Main(string[] args)
+        {
+            int highestValue = Math.Max(13, 12);
+            int lowestValue = Math.Min(44, 22);
+            double squareRoot = Math.Sqrt(25);
+            double roundedNumber = Math.Round(24.5);
+            double absoluteValue = Math.Abs(-5);
+            
+            Console.WriteLine($"The highest number between 13 and 12 is: {highestValue}");
+            Console.WriteLine($"The lowest number between 44 and 2 is: {lowestValue}");
+            Console.WriteLine($"The square root of 25 is: {squareRoot}");
+            Console.WriteLine($"The rounded value of 24.5 is: {roundedNumber}");
+            Console.WriteLine($"The absolute value of -5 is: {absoluteValue}");
+        }
+    }
+}
